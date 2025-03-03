8b67244e663a5fd1f14cdb7f0d015aa9cedf9723
Uma1819 <umaahead@gmail.com>
2025-03-03 17:31:43 +0530

    Update Transaction.sh

--- Transaction.sh
+++ Transaction.sh
@@ -1,1 +1,17 @@
 
+Instance ID
+
+i-00ea051e3ec4f198a (git-machine)
+Open an SSH client.
+
+Locate your private key file. The key used to launch this instance is mum-webserver-.pem-key.pem
+
+Run this command, if necessary, to ensure your key is not publicly viewable.
+chmod 400 "mum-webserver-.pem-key.pem"
+
+Connect to your instance using its Public DNS:
+ec2-52-66-15-79.ap-south-1.compute.amazonaws.com
+
+Example:
+
+ssh -i "mum-webserver-.pem-key.pem" ec2-user@ec2-52-66-15-79.ap-south-1.compute.amazonaws.com
